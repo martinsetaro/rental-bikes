cc5b74f4711c0ffa391f10b5abb6c31a8dbc5447
martinsetaro <lordsesony@gmail.com>
2022-11-21 22:46:35 -0300

    arreglos vista pago

--- components/Payment/Payment.jsx
+++ components/Payment/Payment.jsx
@@ -1,25 +1,28 @@
-import style from './payment.module.css'
-import Image from 'next/image'
-import money from '../../public/assets/img/money.png'
+import style from "./payment.module.css";
+import Image from "next/image";
+import money from "../../public/assets/img/money.png";
 
 const Payment = () => {
-
-
-
   return (
     <div className={style.contenedorPayment}>
-        <h2>Payment methods</h2>
-        <div className={style.contenedorInfoPago}>
-            <div className={style.imagenPago}>
-            <Image src={money} alt="img" width={50} height={50}/>
-           </div>
-        <div className={style.informacionPago}>
-            <p>It is not necessary to make reservations, the payment is made at the moment of signing the contract and after checking everything.
-Payments are accepted in Argentine Pesos, Dollars or Euros at the current exchange rate at the moment of signing the contract (the rate in Dollars or Euros may vary depending on the exchange rate of the day).</p>
+      <h2>Payment methods</h2>
+      <div className={style.contenedorInfoPago}>
+        <div className={style.imagenPago}>
+          <Image src={money} alt="img" width={50} height={50} />
         </div>
-</div>
+        <div className={style.informacionPago}>
+          <p>
+            It is not necessary to make reservations, the payment is made at the
+            moment of signing the contract and after checking everything.
+            Payments are accepted in Argentine Pesos, Dollars or Euros at the
+            current exchange rate at the moment of signing the contract (the
+            rate in Dollars or Euros may vary depending on the exchange rate of
+            the day).
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Payment
+export default Payment;
