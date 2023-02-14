0cf5fed8a7502bfb5994825f40996977979eb149
martinsetaro <lordsesony@gmail.com>
2023-02-14 15:42:53 -0300

    arreglo de footer y header

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -12,10 +12,6 @@ const Footer = () => {
           <Link href="https://www.instagram.com/bcr_mountainbikerental/"><Image src={instagram} alt="instagram" width={50} height={50}/></Link>
            <h3>bcr_mountainbikerental</h3>
         </div>
-        <div className={style.ig}>
-        <Link href="https://www.instagram.com/growingup_mdz/"><Image src={instagram} alt="instagram" width={50} height={50}/></Link>
-           <h3>growingup_mdz/</h3>
-        </div>
         </div>
         <h4>Site designed by <Link href="https://martinsetaro.netlify.app/"><span>Martin Setaro &copy; 2022</span></Link> for Bcr Mountain Rental Mendoza</h4>
     </div>
--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -21,8 +21,8 @@ const Header = () => {
             <Image src={clock} alt="horarios" width={60} height={60}/>
           </div>
           <div className={style.horarios}>
-            <h3 className={style.texto}>Monday to sunday 9:00 a 19:00 hr</h3>
-            <h4 className={style.subTexto}>Open the 365 days of the years </h4>
+            <h3 className={style.texto}>Open 24 hrs</h3>
+            <h4 className={style.subTexto}>The 365 days of the years </h4>
           </div>
         </div>
         
